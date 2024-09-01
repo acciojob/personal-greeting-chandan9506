6fc39e529ccb121a2b72675c9a0fbf1ebfa45b58
Chandan <chandan.ckmodi@gmail.com>
2024-09-01 12:24:23 +0530

    second

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,22 +1,44 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react'
+
+const Greetings = () => {
+    const[name, setName] = useState('')
+  return (
+    <div>
+      <label htmlFor='name'>Enter Your Name:</label><br/>
+      <input id='name' type="text" onChange={(e) => {
+        let greet= e.target.value ? "Hello " + e.target.value +"!" : "";
+        setName(greet)
+
+      }} />
+      <p>{name}</p>
+    </div>
+  )
+}
+
+export default Greetings
 
 
-const Greeting = () => {
-    const [val,setVal] = useState("");
-    return (
-        <>
-       
-        <form>
-            <label>Enter your name:</label><br></br>
-            <input type="text" value={val} onChange={(e) => {setVal(e.target.value)}}/>
-        </form>
-       
-        <div>
-          {val ? <h1>Hello {val}!</h1> : ""}
-        </div>
-        
-        </>
-    );
-};
 
-export default Greeting;
+
+
+
+// import React, { useState } from "react";
+// import './../styles/App.css';
+
+// const App = () => {
+//   const [text, setText] = useState("");
+
+//   return (
+//     <div>
+//       <label htmlFor="name">Enter your name:</label>
+//       <input 
+//         type="text" 
+//         id="name" 
+//         onChange={e => setText(e.target.value)} 
+//       />
+//      <p>{text && text.length>0?"Hello "+ text+"!":""}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
